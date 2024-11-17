42b0505770de301641d2d75cfcc386601cedf799
erie0210 <2j00923@gmail.com>
2024-11-17 22:22:45 +0900

    spring boot 에 생성한 context 를 반영

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.getBean
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
+// @ComponentScan("com.example.demo")
 class DemoApplication
 
 // fun main(args: Array<String>) {
@@ -17,8 +18,16 @@ class DemoApplication
 // }
 
 fun main(args: Array<String>) {
-    val ctx = AnnotationConfigApplicationContext(AppConfig::class.java)
+    val ctx = AnnotationConfigApplicationContext()
+    ctx.register(AppConfig::class.java)
+
+    runApplication<DemoApplication>(*args) {
+        setApplicationContextFactory {
+            ctx
+        }
+    }
+
     val dependency1 = ctx.getBean<Dependency1>()
+    println(ctx.beanDefinitionNames.joinToString("\n"))
     dependency1.doStuff()
-    runApplication<DemoApplication>(*args)
 }
